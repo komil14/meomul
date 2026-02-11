b754e5c20031aae4877cb825824b6f79e87afe53
komil14 <kamilyakub@sju.ac.kr>
2026-02-11 23:04:22 +0900

    feat: created component related modules

--- apps/meomul-api/src/components/components.module.ts
+++ apps/meomul-api/src/components/components.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { HotelModule } from './hotel/hotel.module';
+import { AuthModule } from './auth/auth.module';
+import { ReviewModule } from './review/review.module';
+import { RoomModule } from './room/room.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [MemberModule, HotelModule]
+  imports: [MemberModule, HotelModule, AuthModule, ReviewModule, RoomModule, LikeModule, ViewModule, FollowModule, NotificationModule]
 })
 export class ComponentsModule {}
