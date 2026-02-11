249741958c238d5d0ac21d6ecb5f45ac4dc0be96
komil14 <kamilyakub@sju.ac.kr>
2026-02-11 17:53:02 +0900

    feat: implement database module with Mongoose integration

--- apps/meomul-api/src/database/database.module.ts
+++ apps/meomul-api/src/database/database.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+	imports: [
+		MongooseModule.forRootAsync({
+			useFactory: () => ({
+				uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_DEV,
+			}),
+		}),
+	],
+	exports: [MongooseModule],
+})
+export class DatabaseModule {
+	constructor(@InjectConnection() private readonly connection: Connection) {
+		if (this.connection.readyState === 1) {
+			console.log(
+				'Database connected successfully into ' +
+					(process.env.NODE_ENV === 'production' ? 'PRODUCTION' : 'DEVELOPMENT') +
+					' db',
+			);
+		} else {
+			console.error('Database connection failed');
+		}
+	}
+}
